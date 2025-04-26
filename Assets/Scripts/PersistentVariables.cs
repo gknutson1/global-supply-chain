27deb94a990f27fff096ccfb02ad0bf47ba5e62b
Gunnar Knutson <gunnar.knutson@proton.me>
2025-04-26 14:58:10 -0700

    Update persistent variables

--- Assets/Scripts/PersistentVariables.cs
+++ Assets/Scripts/PersistentVariables.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PersistentVariables : MonoBehaviour
 {
     public int level = 0;
+    public int supply = 0;
+    public List<Ship> ships = new List<Ship>();
 
     void Start()
     {
