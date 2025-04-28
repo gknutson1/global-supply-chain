77457f9dc817daa5c8d14525e488a44d50a0fe05
Collin Deel <collin.deel@gmail.com>
2025-04-27 17:31:22 -0700

    Implement master and SFX volume controls

--- Assets/Menus/Scripts/OptionSlider.cs
+++ Assets/Menus/Scripts/OptionSlider.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionSlider : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    Slider slider;
+    string settingName;
     public float defaultValue;
 
     void Start()
     {
-        GetComponent<Slider>().SetValueWithoutNotify(PlayerPrefs.GetFloat(transform.parent.name, defaultValue));
+        slider = GetComponent<Slider>();
+        settingName = transform.parent.name;
+        var value = PlayerPrefs.GetFloat(settingName, defaultValue);
+        slider.SetValueWithoutNotify(value);
+        if (value > 0) audioMixer.SetFloat(settingName, Mathf.Log10(value) * 40);
+        else audioMixer.SetFloat(settingName, -80);
     }
 
     public void SaveSetting()
     {
-        PlayerPrefs.SetFloat(transform.parent.name, GetComponent<Slider>().value);
+        var value = slider.value;
+        PlayerPrefs.SetFloat(transform.parent.name, value);
+        if (value > 0) audioMixer.SetFloat(settingName, Mathf.Log10(value) * 40);
+        else audioMixer.SetFloat(settingName, -80);
     }
 }
